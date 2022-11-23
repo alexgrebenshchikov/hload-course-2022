f41be91fe72b24ad9e2277cc80d062b788e9bea5
alexgrebenshchikov <qazwsx13240987@gmail.com>
2022-11-23 23:07:41 +0300

    hw2

--- homework-02/dqueue/dqueue.go
+++ homework-02/dqueue/dqueue.go
@@ -1,29 +1,45 @@
 package dqueue
 
 import (
-//    "fmt"
-//    "context"
+	//    "fmt"
+	"context"
+	"errors"
+	"strings"
 
-    "github.com/go-redis/redis/v8"
-//    "github.com/go-zookeeper/zk"
+	"github.com/go-redis/redis/v8"
+	//    "github.com/go-zookeeper/zk"
 
+	"time"
 )
 
 /*
  * Можно создавать несколько очередей
  *
  * Для клиента они различаются именами
- * 
+ *
  * В реализации они могут потребовать вспомогательных данных
- * Для них - эта структура. Можете определить в ней любые поля 
+ * Для них - эта структура. Можете определить в ней любые поля
  */
 type DQueue struct {
+	name                string
+	nShards             int
+	currentShardForPush int
+	shards              []*redis.Client
 }
+
+var all_shards []*redis.Client
+var queues_info map[string]int = make(map[string]int)
+var ctx = context.Background()
+var time_layout = "2006-01-02 15:04:05.999999999 -0700 MST"
 
 /*
  * Запомнить данные и везде использовать
  */
-func Config(redisOptions *redis.Options, zkCluster []string) {
+func Config(redisOptions *[]*redis.Options, zkCluster []string) {
+	all_shards = []*redis.Client{}
+	for _, opts := range *redisOptions {
+		all_shards = append(all_shards, redis.NewClient(opts))
+	}
 }
 
 /*
@@ -40,28 +56,78 @@ func Config(redisOptions *redis.Options, zkCluster []string) {
  *
  * Отдельные узлы Redis-кластера могут выпадать. Availability очереди в целом
  * не должна от этого страдать
- *  
+ *
  */
 func Open(name string, nShards int) (DQueue, error) {
-    return DQueue{}, nil
+	if nShards > len(all_shards) {
+		return DQueue{name, 0, 0, []*redis.Client{}}, errors.New("error: not enough shards")
+	}
+	n_shards_in_storage := queues_info[name]
+
+	if n_shards_in_storage == 0 || n_shards_in_storage == nShards {
+		return DQueue{name, nShards, 0, all_shards[:nShards]}, nil
+	} else {
+		return DQueue{name, 0, 0, []*redis.Client{}}, errors.New("error: queue with this name already exists")
+	}
+
 }
 
 /*
  * Пишем в очередь. Каждый следующий Push - в следующий шард
- * 
+ *
  * Если шард упал - пропускаем шард, пишем в следующий по очереди
  */
-func (*DQueue) Push(value string) error {
-    return nil
+func (q *DQueue) Push(value string) error {
+	for i := 0; i < q.nShards; i++ {
+		var shard = q.shards[q.currentShardForPush]
+		var value_time = value + "::" + time.Now().Format(time_layout)
+		err := shard.LPush(ctx, q.name, value_time).Err()
+		q.currentShardForPush = (q.currentShardForPush + 1) % q.nShards
+		if err == nil || err == redis.Nil {
+			return nil
+		}
+	}
+	return errors.New("error: push value failed")
 }
 
 /*
  * Читаем из очереди
  *
  * Из того шарда, в котором самое раннее сообщение
- * 
+ *
  */
-func (*DQueue) Pull() (string, error) {
-    return "", nil
-}
+func (q *DQueue) Pull() (string, error) {
+	var min_time time.Time
+	var is_min_time_init = false
+	var res string = ""
+	var has_value = false
+	for i := 0; i < q.nShards; i++ {
+		var shard = q.shards[i]
+		str_val, err := shard.RPop(ctx, q.name).Result()
+		if err != nil && err != redis.Nil {
+			continue
+		}
+		if str_val == "" {
+			continue
+		}
 
+		val_and_time := strings.Split(str_val, "::")
+		var val = val_and_time[0]
+		push_time, err := time.Parse(time_layout, val_and_time[1])
+		if err != nil {
+			continue
+		}
+		if !is_min_time_init || push_time.Before(min_time) {
+			min_time = push_time
+			is_min_time_init = true
+			res = val
+			has_value = true
+		}
+
+	}
+	if has_value {
+		return res, nil
+	} else {
+		return "", errors.New("error: pull value failed")
+	}
+}
--- homework-02/main/main.go
+++ homework-02/main/main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
-//    "fmt"
-    "dqueue"
-//    "context"
+	"fmt"
 
-    "github.com/go-redis/redis/v8"
+	//"context"
+
+	"github.com/go-redis/redis/v8"
+
+	//"github.com/go-zookeeper/zk"
+
+	"dqueue"
+
+	"os"
 )
 
 func main() {
-    redisOptions := redis.Options{
-        Addr:     "localhost:6379",
-        Password: "", // no password set
-        DB:       0,  // use default DB
-    }
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) != 4 {
+		fmt.Println("error: wrong number of args - expected 4 addresses of redis shards")
+		return
+	}
+	redisOptions1 := redis.Options{
+		Addr:     argsWithoutProg[0],
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
 
-    dqueue.Config(&redisOptions, []string{"127.0.0.1"})
+	redisOptions2 := redis.Options{
+		Addr:     argsWithoutProg[1],
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
+	redisOptions3 := redis.Options{
+		Addr:     argsWithoutProg[2],
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
+	redisOptions4 := redis.Options{
+		Addr:     argsWithoutProg[3],
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	}
+
+	var redisOptions = []*redis.Options{&redisOptions1, &redisOptions2, &redisOptions3, &redisOptions4}
+
+	dqueue.Config(&redisOptions, []string{"127.0.0.1"})
+	_, err := dqueue.Open("alex-greben:queue1", 4)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
 }
